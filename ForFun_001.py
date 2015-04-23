6e513452a3897c3872f1e32eac39fa1ea0453098
ChengJiunHao <chengjiunhao@gmail.com>
2015-04-23 17:44:49 +0800

    Create ForFun_001.py

---
+++ ForFun_001.py
@@ -0,0 +1,17 @@
+from maya import cmds
+
+cmds.progressWindow(isInterruptable = 1)
+
+while True:
+	WindowCheck = cmds.window(title = 'SHUTDOWN YOUR PC??? MUAHAHAHAHHAHAHA', widthHeight = (400, 500))
+	cmds.paneLayout()
+	cmds.scrollField(wordWrap=True, text='SHUTDOWN YOUR PC??? MUAHAHAHAHHAHAHA')
+	cmds.showWindow(WindowCheck)
+	cmds.showSelectionInTitle(WindowCheck)
+	cmds.deleteUI(WindowCheck)
+	
+	if cmds.progressWindow(query = 1, isCancelled = 1):
+		cmds.progressWindow(endProgress = 1)
+		break
+		
+	cmds.refresh()
